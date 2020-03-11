6039c416fe92131c5584c882a2e3c331b407b250
nekit00748 <61993988+nekit00748@users.noreply.github.com>
2020-03-11 20:32:45 +0300

    C1

---
+++ instruments-publishing/script2.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+	$("#but1").click(function() {
+		$(".divs").css("display","none");
+		$("#div1").show();
+		$("#div1").addClass("animated fadeIn");		
+	})
+	$("#but2").click(function() {
+		$(".divs").css("display","none");
+		$("#div2").show();
+		$("#div2").addClass("animated fadeIn");		
+	})
+	$("#but3").click(function() {
+		$(".divs").css("display","none");
+		$("#div3").show();
+		$("#div3").addClass("animated fadeIn");		
+	})
+	$("#but4").click(function() {
+		$(".divs").css("display","none");
+		$("#div4").show();
+		$("#div4").addClass("animated fadeIn");		
+	})
+	$("#but5").click(function() {
+		$(".divs").css("display","none");
+		$("#div5").show();
+		$("#div5").addClass("animated fadeIn");		
+	})
+	$("#but6").click(function() {
+		$(".divs").css("display","none");
+		$("#div6").show();
+		$("#div6").addClass("animated fadeIn");		
+	})
+	$("#but7").click(function() {
+		$(".divs").css("display","none");
+		$("#div7").show();
+		$("#div7").addClass("animated fadeIn");		
+	})
+	$("#but8").click(function() {
+		$(".divs").css("display","none");
+		$("#div8").show();
+		$("#div8").addClass("animated fadeIn");		
+	})
+
+})
